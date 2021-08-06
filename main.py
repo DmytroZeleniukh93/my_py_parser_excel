e06d5d010a87e216faa609a22f60db5aef8a0056
DmytroZeleniukh93 <d.zeleniukh1@gmail.com>
2021-08-06 15:05:42 +0300

    rel 1.04

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ new_sheet = new_book.active
 # Читає всі комірки з тегами і записує
 def read_html_tags():
     all_tags = []
-    for row in sheet_read2.iter_rows(min_row=5, max_row=15, min_col=2, max_col=4):
+    for row in sheet_read2.iter_rows(min_row=5, max_row=5, min_col=2, max_col=4):
         for cell in row:
             read_cell = cell.value
             all_tags.append(read_cell)
@@ -26,18 +26,28 @@ def read_html_tags():
 all_tags = read_html_tags()
 
 
+def get_clean_price(price):
+    price = price.replace(' ', '')
+    price = price.replace('грн', '')
+    price = price.replace('грн.', '')
+    price = price.replace('\xa0', '')
+    price = price.replace('.', '')
+    price = price.replace('₴', '')
+    return price
+
+
 def calkueted():
     next_column = 2
     next_row = 5
     tag1 = 0
     tag2 = 1
     tag3 = 2
-    for row in sheet_read.iter_rows(min_row=5, max_row=15, min_col=2, max_col=15):
+    for row in sheet_read.iter_rows(min_row=5, max_row=5, min_col=2, max_col=15):
         for cell in row:
             read_cell = cell.value
             if read_cell == 'x':
-                new_sheet.cell(row=next_row, column=next_column).value = 'NO'
-            elif read_cell != 'x':
+                new_sheet.cell(row=next_row, column=next_column).value = 'x' # винести за if
+            else:
                 url = read_cell
                 print(url)
                 try:
@@ -46,20 +56,14 @@ def calkueted():
                     soup = BeautifulSoup(main_text, features="html.parser")
                     price = soup.find(all_tags[tag1], {all_tags[tag2]: all_tags[tag3]})
                     price = price.text
-                    # Забирає з ціни зайві символи
-                    price = price.replace(' ', '')
-                    price = price.replace('грн', '')
-                    price = price.replace('грн.', '')
-                    price = price.replace('\xa0', '')
-                    price = price.replace('.', '')
-                    formula = '=HYPERLINK("' + url + '";"' + price + '")'
+                    price = get_clean_price(price)
+                    formula = f'=HYPERLINK("{url}";"{price}")'
                     new_sheet.cell(row=next_row, column=next_column).value = formula
 
                 except AttributeError:
-                    print('Помилка')
-                    url_not_work = '=HYPERLINK("' + url + '";"' + '!404!' + '")'
+                    print('Error: ' + url)
+                    url_not_work = f'=HYPERLINK("{url}";"!404!")'
                     new_sheet.cell(row=next_row, column=next_column).value = url_not_work
-
 
             next_column += 1
 
@@ -69,6 +73,7 @@ def calkueted():
                 tag1 += 3
                 tag2 += 3
                 tag3 += 3
+
 
 calkueted()
 new_book.save('new_price_url.xlsx')
