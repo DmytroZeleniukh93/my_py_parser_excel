89c0563d3d9befbe9c310c8d095f14408a485df1
DmytroZeleniukh93 <d.zeleniukh1@gmail.com>
2021-08-22 21:24:43 +0300

    rel 1.16

--- graphic_interface.py
+++ graphic_interface.py
@@ -6,39 +6,60 @@ from main import ScrapPrices
 class Window(ScrapPrices):
     def __init__(self):
         super().__init__()
-        self.canvas = Tk()
-        self.canvas.title('Перевірка цін з конкурентами')
-        self.canvas.geometry('600x400+500+200')
-        self.canvas.resizable(True, True)
-        self.label_url = Label(self.canvas)
-        Label(self.canvas, text='Info: ').place(x=30, y=30)
-        self.pb = Progressbar(self.canvas, length=534)
-        Label(self.canvas, text='Початок рядка: ').place(x=30, y=160)
-        default_start_row = StringVar(self.canvas, value='4')
-        self.start_row = Entry(self.canvas, width=8, textvariable=default_start_row)
-        Label(self.canvas, text='Початок колонки: ').place(x=197, y=100)
-        self.start_col = Entry(self.canvas, width=16).place(x=200, y=120)
-        Label(self.canvas, text='Кінець колонки: ').place(x=310, y=100)
-        self.start_col = Entry(self.canvas, width=16).place(x=313, y=120)
-        Label(self.canvas, text='Кінець рядка: ').place(x=30, y=200)
-        self.start_end = Entry(self.canvas, width=8).place(x=130, y=200)
+        self.root = Tk()
+        self.root.title('Перевірка цін з конкурентами')
+        self.root.geometry('600x400+500+200')
+        self.root.resizable(True, True)
+        self.canvas = Canvas(self.root)
+        self.my_image = PhotoImage(file='excel_png.png')
+        self.canvas.create_image(210, 190, image=self.my_image)
+        self.label_url = Label(self.root)
+        Label(self.root, text='Info: ').place(x=30, y=30)
+        self.pb = Progressbar(self.root, length=534)
+        Label(self.root, text='Почати з ряду: ').place(x=30, y=197)
+        default_start_row = StringVar(self.root, value='4')
+        self.start_row = Entry(self.root, width=8, textvariable=default_start_row)
+        Label(self.root, text='Закінчити рядом: ').place(x=30, y=277)
+        default_end_row = StringVar(self.root, value='26')
+        self.end_row = Entry(self.root, width=7, textvariable=default_end_row)
+        Label(self.root, text='Почати з колонки: ').place(x=234, y=120)
+        default_start_col = StringVar(self.root, value='2')
+        self.start_col = Entry(self.root, width=13, textvariable=default_start_col)
+        Label(self.root, text='Закінчити колонкою: ').place(x=404, y=120)
+        default_end_col = StringVar(self.root, value='16')
+        self.end_col = Entry(self.root, width=13, textvariable=default_end_col)
 
     def run(self):
         self.draw_widgets()
-        self.canvas.mainloop()
+        self.root.mainloop()
 
     def draw_widgets(self):
-        Button(self.canvas, width=10, height=1, text='Пуск', command=self.button_action).place(x=487, y=100)
+        self.canvas.place(x=135, y=50)
         self.label_url.place(x=60, y=30)
         self.pb.place(x=33, y=50)
-        self.start_row.place(x=130, y=160)
+        Button(self.root, width=10, height=1, text='Пуск', command=self.button_action).place(x=487, y=90)
+        self.start_row.place(x=134, y=197)
+        self.end_row.place(x=134, y=277)
+        self.start_col.place(x=237, y=140)
+        self.end_col.place(x=407, y=140)
 
     def button_action(self):
-        self.get_tags(4, 5)
-        self.get_result(4, 5, 2, 7) # порахувати колонки і стовпці і їх помножити
+        self.get_nums()
+        self.test()
+        self.get_tags(self.s_row, self.e_row)
+        self.get_result(self.s_row, self.e_row, self.s_col, self.e_col)
         self.save()
-        print(self.start_row.get())
 
+    def get_nums(self):
+        self.s_row = int(self.start_row.get())
+        self.e_row = int(self.end_row.get())
+        self.s_col = int(self.start_col.get())
+        self.e_col = int(self.end_col.get())
+
+    def test(self):
+        x = (self.e_row - self.s_row) + 1
+        y = (self.e_col - self.s_col) + 1
+        self.res = 100 / (x * y)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from bs4 import BeautifulSoup
 
 class ScrapPrices:
     replace_list = [' ', 'грн', '.', 'грн.', '\xa0', '₴', '\n', '\t', 'Цена', ':']
+    all_tags = []
 
     def __init__(self):
         self.read_settings = openpyxl.open('settings.xlsx', read_only=True)
         self.sheet_settings = self.read_settings.active
-        self.all_tags = []
         self.read_shop_url = openpyxl.open('shop_url.xlsx', read_only=True)
         self.shop_url = self.read_shop_url.active
         self.new_book = openpyxl.Workbook()
@@ -27,7 +27,8 @@ class ScrapPrices:
         tag1 = 0
         tag2 = 1
         tag3 = 2
-        v = 8.33
+        v = self.res
+        vv = v
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         for row in self.shop_url.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
             for cell in row:
@@ -42,7 +43,6 @@ class ScrapPrices:
                     self.label_url.configure(text=url)
                     self.label_url.update()
 
-                    '''
                     try:
                         source = requests.get(url, headers=headers)
                         main_text = source.content.decode()
@@ -55,9 +55,9 @@ class ScrapPrices:
                         print('Error: ' + url)
                         to_write = f'=HYPERLINK("{url}";"!404!")'
                         self.new_sheet.cell(row=start_row, column=start_col).value = to_write
-                    '''
+
                 start_col += 1
-                v += 8.33
+                v += vv
                 if start_col == end_col + 1:
                     start_row += 1
                     start_col = 2
@@ -79,102 +79,3 @@ class ScrapPrices:
         self.new_book.save('class_price_url.xlsx')
         self.new_book.close()
         self.__close_files()
-
-'''
-if __name__ == "__main__":
-    scrap_price = ScrapPrices()
-    scrap_price.get_tags(5, 5)
-    scrap_price.get_result(5, 5, 2, 4)
-    scrap_price.save()
-
-
-# Відкриває існуючий xlsx файл з url
-read_book = openpyxl.open('shop_url.xlsx', read_only=True)
-sheet_read = read_book.active
-# Відкриває файл з html тегами
-read_book2 = openpyxl.open('settings.xlsx', read_only=True)
-sheet_read2 = read_book2.active
-# Створює новий xlsx файл в який буде записоно нові дані
-new_book = openpyxl.Workbook()
-new_sheet = new_book.active
-
-start_row = int(input('Початковий рядок: '))
-end_row = int(input('Кінцейвий рядок: '))
-start_col = int(input('Початкова колонка: '))
-end_col = int(input('Кінцева колонка: '))
-
-# Читає всі комірки з тегами і записує
-def read_html_tags(start_row, end_row):
-    all_tags = []
-    for row in sheet_read2.iter_rows(min_row=start_row, max_row=end_row, min_col=2, max_col=4):
-        for cell in row:
-            read_cell = cell.value
-            all_tags.append(read_cell)
-    return all_tags
-
-
-all_tags = read_html_tags(start_row, end_row)
-
-
-def get_clean_price(price):
-    price = price.replace(' ', '')
-    price = price.replace('грн', '')
-    price = price.replace('грн.', '')
-    price = price.replace('\xa0', '')
-    price = price.replace('.', '')
-    price = price.replace('₴', '')
-    price = price.replace('\n', '')
-    price = price.replace('\t', '')
-    price = price.replace('.', '')
-    price = price.replace(':', '')
-    price = price.replace('Цена', '')
-    return price
-
-
-def result(start_row, end_row, start_col, end_col):
-    #active_column = 2
-    tag1 = 0
-    tag2 = 1
-    tag3 = 2
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    for row in sheet_read.iter_rows(min_row=start_row, max_row=end_row, min_col=start_col, max_col=end_col):
-        for cell in row:
-            read_cell = cell.value
-            if read_cell == 'x':
-                new_sheet.cell(row=start_row, column=start_col).value = 'x'  # винести за if
-            else:
-                url = read_cell
-                print(url)
-                try:
-                    source = requests.get(url, headers=headers)
-                    main_text = source.content.decode()
-                    soup = BeautifulSoup(main_text, features="html.parser")
-                    price = soup.find(all_tags[tag1], {all_tags[tag2]: all_tags[tag3]})
-                    price = price.text
-                    price = get_clean_price(price)
-                    formula = f'=HYPERLINK("{url}";"{price}")'
-                    new_sheet.cell(row=start_row, column=start_col).value = formula
-
-                except BaseException:
-                    print('Error: ' + url)
-                    url_not_work = f'=HYPERLINK("{url}";"!404!")'
-                    new_sheet.cell(row=start_row, column=start_col).value = url_not_work
-
-            start_col += 1
-
-            if start_col == 17:  # пофіксити
-                start_row += 1
-                start_col = 2
-                tag1 += 3
-                tag2 += 3
-                tag3 += 3
-
-
-result(start_row, end_row, start_col, end_col)
-
-name_file = time.asctime()
-name_file = name_file[3:10]
-
-new_book.save(f'price_url {name_file}.xlsx')
-new_book.close()
-'''
